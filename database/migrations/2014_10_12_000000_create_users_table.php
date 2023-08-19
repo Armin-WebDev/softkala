62d3c24d73ae2265f43aaf379be76bebedcb457f
ArminP <ArminPouralvani@outlook.com>
2023-08-19 12:32:11 +0330

    migrated users table and added some extra field to it

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,9 +13,17 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
+
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('last_name');
+            $table->string('phone');
+            $table->string('national_code');
+            $table->string('birthday');
+            $table->tinyInteger('gender');
+            $table->string('bank_number');
+            $table->unsignedBigInteger('province_code');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
