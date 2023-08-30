347423f6282ab27e7b5fe6b20a4e17afaf47897a
ArminP <ArminPouralvani@outlook.com>
2023-08-30 13:35:14 +0330

    fixed attributes index blade

--- app/Models/AttributeGroup.php
+++ app/Models/AttributeGroup.php
@@ -7,5 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class AttributeGroup extends Model
 {
+    protected $table = 'attributesgroup';
     use HasFactory;
 }
--- resources/views/admin/attributes/index.blade.php
+++ resources/views/admin/attributes/index.blade.php
@@ -30,9 +30,9 @@
         </section>
         <div class="card dark-mode">
             <div class="card-header border-transparent">
-                <h3 class="card-title">دسته بندی ها</h3>
+                <h3 class="card-title">ویژگی ها</h3>
                 <div class="card-tools text-left">
-                    <a href="{{ route('categories.create') }}" class="btn btn-app">
+                    <a href="{{ route('attributes-group.create') }}" class="btn btn-app">
                         <i class="fa fa-plus"></i> جدید
                     </a>
                 </div>
@@ -46,6 +46,7 @@
 
                             <th class="text-center">شناسه</th>
                             <th class="text-center">عنوان</th>
+                            <th class="text-center">نوع</th>
                             <th class="text-center">عملیات</th>
 
 
@@ -53,16 +54,17 @@
                         </thead>
                         <tbody>
 
-                        @foreach($categories as $category)
+                        @foreach($attributesGroup as $attribute)
 
                             <tr>
 
-                                <td class="text-center"> {{ $category->id }}</td>
-                                <td>{{ $category->name }}</td>
+                                <td class="text-center"> {{ $attribute->id }}</td>
+                                <td>{{ $attribute->name }}</td>
+                                <td>{{ $attribute->type }}</td>
                                 <td class="text-center">
-                                    <a class="btn btn-warning" href="{{ route('categories.edit' , $category->id) }}">ویرایش</a>
+                                    <a class="btn btn-warning" href="{{ route('categories.edit' , $attribute->id) }}">ویرایش</a>
                                     <div style="display: inline-block">
-                                        <form method="post" action="/administrator/categories/{{$category->id}}">
+                                        <form method="post" action="/administrator/categories/{{$attribute->id}}">
                                             @csrf
                                             <input type="hidden" name="_method" value="DELETE">
                                             <button type="submit" class="btn btn-danger">حذف</button>
